dd326889a3941784644aa7e223a14290904016b8
Alexander Ege <alexander@ege.com>
2019-11-01 18:04:25 -0700

    Custom code snippets

--- apps/note_app/static/note_app/js/script.js
+++ apps/note_app/static/note_app/js/script.js
@@ -59,6 +59,8 @@ function toggleEdit(note_comment_id){
     console.log("Toggling Edit");
     console.log(note_comment_id);
     var parent = document.getElementById('note_comment_' + note_comment_id);
+    console.log("Parent: ", parent);
+    console.log("Sibling: ", parent.nextElementSibling);
     if (parent.nextElementSibling.style.display === "none") {
         parent.style.display = "none";
         parent.nextElementSibling.style.display = "block";
@@ -68,8 +70,48 @@ function toggleEdit(note_comment_id){
     }
 }
 
-function codeSnippet(){
+function toggleCodeEdit(note_comment_id){
+    console.log("Toggling Code Edit");
+    console.log(note_comment_id);
+    var parent = document.getElementById('note_comment_' + note_comment_id);
+    console.log("Parent: ", parent);
+    console.log("Parent: ", parent.parentElement);
+    if (parent.parentElement.nextElementSibling.style.display === "none") {
+        parent.style.display = "none";
+        parent.parentElement.nextElementSibling.style.display = "block";
+    } else {
+        parent.style.display = "block";
+        parent.parentElement.nextElementSibling.style.display = "none";
+    }
+}
 
+// https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+function copyToClipboard(ele){
+
+    const el = document.createElement('textarea');
+    el.value = ele.parentNode.innerText;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    // console.log("ELEMENT: ", ele.parentElement)
+
+    // /* Get the text field */
+    // var copyText = ele.parentNode.innerHTML;
+    // var copyTextNode = ele.parentElement.parentElement.parentElement;
+    // console.log(copyTextNode)
+    // ele.select()
+    
+    // // /* Select the text field */
+    // // copyText.select();
+    // // copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    
+    // /* Copy the text inside the text field */
+    // document.execCommand("copy");
+    
+    // /* Alert the copied text */
+    // alert("Copied the text: " + copyText);
 }
 
 $(document).ready(function(){
@@ -103,8 +145,6 @@ wrapper.setAttribute('class', "language-css");
 // for (i = 0; i < el.length; i++){
 //     code_elements[i].parentNode.classList.add("language-css");
 // }
-
-
 
 
 
@@ -170,8 +210,8 @@ var toggle = true;
 function openNav() {
   if (toggle){
     document.getElementById("toggle_button").innerHTML='<i class="fas fa-chevron-left" id="toggle_button"></i>';
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "20vw";
+    document.getElementById("main").style.marginLeft = "20vw";
     toggle = false;
   } else {
     document.getElementById("toggle_button").innerHTML='<i class="fas fa-chevron-right" id="toggle_button"></i>';
