b0a7007983679c9c7c36e3aa6cab8513446245b8
Alexander Ege <alexander@ege.com>
2019-12-04 11:03:00 -0800

    More css organization

--- apps/note_app/static/note_app/js/script.js
+++ apps/note_app/static/note_app/js/script.js
@@ -1,3 +1,22 @@
+// // Search bar functionality
+function searchFunction() {
+    // var input, filter, ul, li, a, i, txtValue;
+    // input = document.getElementById("myInput");
+    // filter = input.value.toUpperCase();
+    // list = document.getElementById("list");
+    // li = list.getElementsByClassName("accordion")
+    // for (i = 0; i < li.length; i++) {
+    //     a = li[i].getElementsByClassName("toggle")[0];
+    //     txtValue = a.textContent || a.innerText;
+    //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    //         li[i].style.display = "";
+    //     } else {
+    //         li[i].style.display = "none";
+    //     }
+    // }
+}
+
+
 // Show/Hide Navbar Dropdown
 
 /* When the user clicks on the button, 
